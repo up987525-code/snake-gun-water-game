652d0db37d8a7f8df65f25c74f89bd08227ff46f
Utkarsh Pandey <up987525@gmail.com>
2026-09-10 19:39:28 +0530

    Create snake gun water.py

---
+++ snake gun water.py
@@ -0,0 +1,29 @@
+import random
+def check(comp,user):
+    if comp==user:
+        return 0
+    if (comp==0 and user==1):
+        return -1
+    if (comp==2 and user==1):
+        return -1
+    if (comp==2 and user==0):
+         return -1
+    return 1
+while True:
+
+     comp=random.randint(0,2)
+     user=int(input("0 for snake 1 for water 2 for gun ya 9 exit the game:\n"))
+     if user==9:
+         print("bye bye exit the game")
+         break
+     score=check(comp,user)
+
+     print ("you: ", user)
+     print ("computer: ",comp)
+
+     if score== 0:
+       print ("draw")
+     elif score==-1:
+      print("you lose")
+     else:
+       print("you win")
